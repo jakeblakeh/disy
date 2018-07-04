97af73f09a6f72dfe24a997995b58fe1d9b65570
jakeblakeh <35850116+jakeblakeh@users.noreply.github.com>
2018-07-04 14:48:30 -0700

    Update idex.js

--- idex.js
+++ idex.js
@@ -7,7 +7,7 @@ const prefix = "!"
 
 Client.on("ready", () => {
 	console.log("online");
-	Client.user.setPresence({ game: { name: `A Server`, type: 0} });
+	Client.user.setPresence({ game: { name: `Disy Official Bot`, type: 0} });
 });
 
 // welcome message
